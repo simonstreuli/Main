65b1d7a97f3fcaafbdb1f360467b2bd971d4e2dc
Saša Acimovic <sasa.acimovic@slgbroadcast.com>
2023-10-06 11:28:20 +0200

    Login Page

--- frontend/social-media-app/src/App.js
+++ frontend/social-media-app/src/App.js
@@ -1,12 +1,13 @@
 import Register from "../src/pages/register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
-    
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
